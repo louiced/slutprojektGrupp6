46d1206c86e5de55c80f8e1a8ecfd71dd2aebe35
Martin Larsson <martin.larsson.post@gmail.com>
2017-10-10 16:36:56 +0200

    LogoutBtn and filter works

--- frontend/src/ViewSelector.js
+++ frontend/src/ViewSelector.js
@@ -20,27 +20,38 @@ class ViewSelector extends React.Component{
 					id: 'tab2'
 				}
 			],
-			view: 'Login'
+			view: 'Login',
+			loggedInAs: null
 		}
 		this.ccLoginClick = this.ccLoginClick.bind(this);
 		this.logOut = this.logOut.bind(this);
 		this.switchTab = this.switchTab.bind(this);
 		this.updateView = this.updateView.bind(this);
 		this.updateUserId = this.updateUserId.bind(this);
+		this.logOutClick = this.logOutClick.bind(this);
+		this.updateUserInfo = this.updateUserInfo.bind(this);
 	}
 	render(){
 		let view;
+		let logOutBox;
+		if (this.state.loggedInAs !== null){
+			logOutBox = <div className="userInfo">
+				<button className="btn" onClick={this.logOutClick}>LOGGA UT</button>
+				<p>Inloggad som {this.state.loggedInAs.name.first} {this.state.loggedInAs.name.last}</p>
+			</div>
+		} else {
+			logOutBox = <div>LOGUTBOXJÄVEL</div>
+		}
+		
 		let navBar = <ul className="navBar">
 					<li><span className={this.state.tabs[0].class} onClick={this.switchTab} id="tab1">Logga in</span></li>
 					<li><span className={this.state.tabs[1].class} onClick={this.switchTab} id="tab2">Registrera</span></li>
 				</ul>;
 		switch (this.state.view){
-			case 'Login': view =
-				<div className="mainContent">
-					{navBar}
-					<LoginComponent updateView={this.updateView} updateUserId={this.updateUserId} updateUserInfo={this.props.updateUserInfo}/>
-				</div>
-
+			case 'Login': view = <div className="mainContent">
+						{navBar}
+						<LoginComponent updateView={this.updateView} updateUserId={this.updateUserId} updateUserInfo={this.updateUserInfo}/>
+					</div>
 				break;
 			case 'registerNewCC': view = <div className="mainContent">
 			{navBar}
@@ -48,7 +59,7 @@ class ViewSelector extends React.Component{
 				</div>
 			break;
 
-			case 'UserView': view = <div className="mainContent"><UserView userId={this.state.userId}/></div>
+			case 'UserView': view = <div>{logOutBox}<div className="mainContent"><UserView userId={this.state.userId}/></div></div>
 			break;
 			case 'AdminView': view = <div className="mainContent"><AdminView/></div>
 
@@ -99,6 +110,18 @@ class ViewSelector extends React.Component{
 		this.setState({
 			userId: str
 		});
+	}
+	
+	logOutClick(ev){
+		this.setState({
+			view: 'Login'
+		});
+	}
+	
+	updateUserInfo(user) {
+		this.setState({
+			loggedInAs: user
+		})
 	}
 }
 
